73477f53b81929aefb0046076f637088a50a3f56
Fernando Monteiro <fernando@newaeonweb.com.br>
2013-01-03 15:59:52 -0200

    add Scott Jehl script
    to hide url bar on Android and Iphone

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,45 @@
 */
 
 //Hide the url bar on smartphones
-body.onload = function() {
-  window.setTimeout(function() {
-    window.scrollTo(0, 1);
-}, 0);
+/*
+  * Normalized hide address bar for iOS & Android
+  * (c) Scott Jehl, scottjehl.com
+  * MIT License
+*/
+(function( win ){
+  var doc = win.document;
+  
+  // If there's a hash, or addEventListener is undefined, stop here
+  if( !location.hash && win.addEventListener ){
+    
+    //scroll to 1
+    window.scrollTo( 0, 1 );
+    var scrollTop = 1,
+      getScrollTop = function(){
+        return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+      },
+    
+      //reset to 0 on bodyready, if needed
+      bodycheck = setInterval(function(){
+        if( doc.body ){
+          clearInterval( bodycheck );
+          scrollTop = getScrollTop();
+          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+        } 
+      }, 15 );
+    
+    win.addEventListener( "load", function(){
+      setTimeout(function(){
+        //at load, if user hasn't scrolled more than 20 or so...
+        if( getScrollTop() < 20 ){
+          //reset to hide addr bar at onload
+          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+        }
+      }, 0);
+    } );
+  }
+})( this );
+
 //Simple function to a responsive menu
    $(function() {
       // Create the dropdown base
