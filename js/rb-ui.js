4663f79b09432f72517f355af637feb26b15d520
Fernando Monteiro <fernando@newaeonweb.com.br>
2013-09-01 23:15:30 -0300

    Major update, including a UI interface components for menu and accordion.

--- js/rb-ui.js
+++ js/rb-ui.js
@@ -20,36 +20,6 @@
 })(jQuery);
 // To initiate this function use $('some element').Rbmenu(); Check the custom.js for a better understand.
 
-// Rb Tabs, Depends on ui-tabs.less/or use ui-tab.css on a separate environment
-(function () {
-    'use strict';
-
-    $.extend($.fn, {
-        Rbtabs: function () {
-
-            $(this).find('div').eq(0).addClass('listTabs');
-            $(this).find('div').eq(1).addClass('contentTabs');
-
-            var tabsList = '.listTabs';
-            var tabContent = '.contentTabs';
-
-            $(tabContent + ' div').hide();
-            $(tabContent + ' div:first-child').show();
-
-            $(tabsList + ' a').on('click', function () {
-                $(tabsList + ' a').removeClass('selected');
-                $(this).addClass('selected');
-
-                $(tabContent + ' div').hide();
-
-                $(tabContent + ' ' + $(this).attr('href')).show();
-                return false;
-            });
-        }
-    });
-})(jQuery);
-// To initiate this function use $('some element').Rbtabs(); Check the custom.js for a better understand.
-
 // Rb Accordion, Depends on ui-accordion.less/or use ui-accordion.css on a separate environment
 (function () {
     'use strict';
@@ -72,27 +42,3 @@
     });
 })(jQuery);
 // To initiate this function use $('some element').Rbaccordion(); Check the custom.js for a better understand.
-
-// Rb Modal, Depends on ui-modal.less/ or use ui-modal.css on a separate environment
-(function () {
-    'use strict';
-
-    $.extend($.fn, {
-        Rbmodal: function () {
-            //$('.Rb-modal').hide();
-            $('#open-modal').on('click', function () {
-                $('<div id="overlay" style="width:1269px;height:780px; background-color:rgba(0,0,0,0.3); position: fixed; top:0;left:0; z-index:2000"></div>').appendTo('body');
-                $(".Rb-modal").animate({
-                    top: '+50'
-                }, 100);
-            });
-            $('#close-modal').on('click', function () {
-                $('#overlay').remove();
-                $(".Rb-modal").animate({
-                    top: '-500'
-                }, 100);
-            });
-        }
-    });
-})(jQuery);
-// To initiate this function use $('some element').Rbmodal(); Check the custom.js for a better understand.
